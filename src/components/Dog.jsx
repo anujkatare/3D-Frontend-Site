0481e68014c5c750ac81d600132268d2908394cb
anujkatare <anujkatare2006@gmail.com>
2026-03-05 01:14:19 +0530

    Added animations and Content

--- src/components/Dog.jsx
+++ src/components/Dog.jsx
@@ -1,9 +1,17 @@
 import { useGLTF, useTexture, useAnimations } from '@react-three/drei'
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 import * as THREE from 'three'
 import { useThree } from '@react-three/fiber'
 
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
 const Dog = () => {
+
+  gsap.registerPlugin(useGSAP());
+  gsap.registerPlugin(ScrollTrigger)
+
   const model = useGLTF(
   "/models/dog.drc.glb", 
   "https://www.gstatic.com/draco/versioned/decoders/1.5.5/"
@@ -39,13 +47,49 @@ const Dog = () => {
     })
   }, [model, normalMap, sampleMatCap, gl])
 
+  const dogModel = useRef(model)
+
+  useGSAP(() => {
+     const tl = gsap.timeline({
+      scrollTrigger:{
+        trigger: "#section-1",
+        endTrigger: "#section-3",
+        start:" top top",
+        end: "bottom bottom",
+        markers: true,
+        scrub: true
+      }
+     })
+
+     tl
+     .to(dogModel.current.scene.position,{
+      z:"-3.5"
+     })
+     .to(dogModel.current.scene.rotation,{
+      x:"0.1"
+     })
+     .to(dogModel.current.scene.rotation,{
+      y:"-2"
+     }, "third")
+     .to(dogModel.current.scene.position,{
+      x:"-3"
+     }, "third")
+     .to(dogModel.current.scene.rotation,{
+      x:"0.5"
+     }, "third")
+     .to(dogModel.current.scene.scale,{
+      x:11.5,
+      y:11.5,
+      z:11.5
+     }, "third")
+
+  }, [])
+
   return (
-    <primitive
-      object={model.scene}
-      scale={5}
-      position={[1, -2, 0]}
-      rotation={[-0.3, 0.8, 0]}
-    />
+    <>
+    <primitive object={model.scene} scale={11} position={[ 2.3, -5.9, 0 ]} rotation={[ 0, Math.PI / 5.2, 0 ]} />
+            <directionalLight position={[ 0, 5, 5 ]} color={0x171717} intensity={-10} />
+    </>
   )
 }
 
